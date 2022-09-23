193923d6258640c65f6816947cf78ffd6340281b
林佳涛 <lne128gene980@163.com>
2022-09-23 13:47:12 +0800

    feat: init layout

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,10 +1,49 @@
 <script setup lang="ts">
+import { GRID_HEIGHT, GRID_WIDTH, P_ALIVE } from '@/app/variable'
 
+interface Ceil {
+  row: number
+  col: number
+  isAlive: boolean
+}
+
+const ceils = ref<Ceil[][]>(gene())
+
+function gene(): Ceil[][] {
+  return Array.from({ length: GRID_HEIGHT }, (_, row) => Array.from({ length: GRID_WIDTH }, (_, col) => ({
+    isAlive: Math.random() < P_ALIVE,
+    row,
+    col,
+  })))
+}
+
+let frame = 0
+function animate() {
+  requestAnimationFrame(() => {
+    frame++
+    if (frame === 30) {
+      ceils.value = gene()
+      frame = 0
+    }
+    animate()
+  })
+}
+animate()
 </script>
 
 <template>
   <div h-100vh w-full flex="c col">
-    Game of Life
+    <h3 mb-4 text="30px" font="bold mono">
+      Game of Life
+    </h3>
+    <div border="~ black">
+      <div v-for="row in ceils.length" :key="row" flex="~">
+        <div v-for="col in ceils[0].length" :key="col" w-14px h-14px flex="c">
+          <div v-show="ceils[row - 1][col - 1].isAlive" w-14px h-14px bg="black" />
+          <div v-show="!ceils[row - 1][col - 1].isAlive" w-14px h-14px bg="white" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
