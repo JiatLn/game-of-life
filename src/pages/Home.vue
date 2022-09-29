db165fdd7ea7d9d818941f9267194ac610f2b236
林佳涛 <lne128gene980@163.com>
2022-09-29 16:07:46 +0800

    feat: add pause/play btn

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -15,11 +15,12 @@ function gene(): Ceil[][] {
 
 const progenyNum = ref<number>(0)
 const aliveNum = ref<number>(0)
+const isPause = ref<boolean>(false)
 let frame = 0
 function animate() {
   requestAnimationFrame(() => {
     frame++
-    if (frame === 30) {
+    if (!isPause.value && frame >= 30) {
       ceils.value = updateState(ceils.value)
       aliveNum.value = getAliveNum(ceils.value)
       progenyNum.value++
@@ -39,7 +40,7 @@ function toGithub() {
   <div h-100vh w-full flex="c col">
     <h3 mb-4 text="30px" font="bold mono" flex="c gap-20px">
       <div><span text="orange">Game</span>Of<span text="green">Life</span></div>
-      <div i-carbon-logo-github text="black" @click="toGithub" cursor-pointer hover="text-orange" />
+      <div i-carbon-logo-github text="black" cursor-pointer hover="text-orange" @click="toGithub" />
     </h3>
     <div flex="c gap-20px" mb-4>
       <div>
@@ -49,11 +50,15 @@ function toGithub() {
         Alive: <span font="bold" text="green">{{ aliveNum }}</span>
       </div>
     </div>
+    <button btn mb-4 flex="c gap-4px" @click="isPause = !isPause">
+      <div v-show="!isPause" i-ant-design:pause-circle-twotone />
+      <div v-show="isPause" i-ant-design:play-circle-twotone />
+      <span>{{ !isPause ? 'Pause' : 'Play!' }}</span>
+    </button>
     <div border="~ gray">
-      <div v-for="row in ceils.length" :key="row" flex="~">
-        <div v-for="col in ceils[0].length" :key="col" w-14px h-14px flex="c">
-          <div v-show="ceils[row - 1][col - 1].isAlive" w-14px h-14px bg="black" />
-          <div v-show="!ceils[row - 1][col - 1].isAlive" w-14px h-14px bg="white" border />
+      <div v-for="(row, idx) in ceils" :key="idx" flex="~">
+        <div v-for="{ col, isAlive } in row" :key="col" w-14px h-14px flex="c">
+          <div w-full h-full border transition="all duration-300ms" :class="isAlive ? 'bg-black' : 'bg-white'" />
         </div>
       </div>
     </div>
